e8a06ecb1ab002568e451332c33fa15596ea1149
Hassan Raza <141661553+HassanRaza-hr@users.noreply.github.com>
2024-01-24 00:58:47 +0500

    home page responsiveness done 100%

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,18 +7,18 @@ function NavBar() {
   const [navbar, setNavbar] = useState(false);
   return (
     <div>
-      <nav className="w-full bg-black fixed top-0 left-0 right-0 z-10">
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+      <nav className="w-full sticky  bg-white top-0 left-0 right-0 z-10">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 ml-5">
           <div>
-            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="flex items-center justify-between py-3 md:py-5 md:block ">
               {/* LOGO */}
               <div className='flex '>
-                <div>
+                <div className='m-auto'>
                   <Image src="/img/logo1.png" alt='logo' width={50} height={50}/>
                 </div>
                 <div className=''>
               <Link href="/">
-                <h2 className="text-2xl text-cyan-600 font-bold ">HASSAN</h2>
+                <h2 className="text-2xl text-cyan-600 font-bold mt-2 ml-4">HASSAN</h2>
                 
               </Link>
               </div>
@@ -46,33 +46,32 @@ function NavBar() {
           </div>
           <div>
             <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0  ${
                 navbar ? 'p-12 md:p-0 block' : 'hidden'
-              }`}
-            >
+              }`}>
               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#about" onClick={() => setNavbar(!navbar)}>
+                <li className="pb-6 text-xl text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#home" onClick={() => setNavbar(!navbar)}>
                     Home
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#blog" onClick={() => setNavbar(!navbar)}>
+                <li className="pb-6 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#contact" onClick={() => setNavbar(!navbar)}>
                     Contact
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#contact" onClick={() => setNavbar(!navbar)}>
+                <li className="pb-6 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="#about" onClick={() => setNavbar(!navbar)}>
                     About
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="#projects" onClick={() => setNavbar(!navbar)}>
                     Projects
                   </Link>
                 </li>
                 <li>
-                <button className='mb-3 box decoration-clone  bg-gradient-to-r from-indigo-600 to-black rounded-full text-[23px] p-2 ml-10'>Download CV</button>
+                <button className='mb-3 font-semibold  box decoration-clone  bg-gradient-to-r from-cyan-600 to-black rounded-full text-[23px] p-2 ml-10'>Download CV</button>
                 </li>
               </ul>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,60 @@
-
-import Navbar from "./components/navbar"
+import Image from "next/image";
+import Navbar from "./components/navbar";
+import { MdWavingHand } from "react-icons/md";
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <section id="home" className=" text-white h-screen bg-purple-900 flex">
-        <h1 className="m-auto font-waterfall text-7xl text-white font-bold">
-          Home
-        </h1>
+      <section id="home" className=" text-black h-screen bg-white">
+        <div className="grid grid-cols-1 md:grid-cols-2 ">
+          <div className="  grid items-center justify-center sm:py-[50px] ">
+            <div className="mb-40 ml-20 mr-10 sm:py-[110px] md:h-screen md:items-center md:justify-center">
+              <div className="text-[20px] text-blue-500 flex ">
+                Hey There
+                <span className="text-yellow-600">
+                  <MdWavingHand />
+                </span>{" "}
+                I am
+              </div>
+              <div className="font-semibold text-[50px]">Hassan Raza</div>
+              <div className="text-[20px]">
+                {" "}
+                Professional Fullstack developer
+              </div>
+              <div className="">
+                  I've strong hands on the website devoplment using next.js 14
+                using typescript.I'm qualified to develop optimized and high
+                performance web app.
+              </div>
+              <div className="p-5">
+              <button className='mb-3 text-white font-semibold  box decoration-clone  bg-gradient-to-r from-cyan-600 to-black rounded-full text-[23px] p-2 ml-10'>About me</button>
+              <button className='mb-3  text-white font-semibold  box decoration-clone  bg-gradient-to-r from-cyan-600 to-black rounded-full text-[23px] p-2 ml-10'>Contact me</button>
+              </div>
+            </div>
+          </div>
+
+          <div className=" md:h-screen md:flex justify-center md:py-[40px]">
+            <div>
+              <Image
+                className="flex justify-center ml-11 md:py-[100px] "
+                src="/img/pic.png"
+                alt="Profile pic"
+                width={300}
+                height={150}
+              />
+            </div>
+          </div>
+        </div>
+        {/* <div className="grid grid-cols-2">
+          <div>
+            <div className="text-xl flex px-[200px] ">Hey There<span className="text-yellow-600"><MdWavingHand /></span> I am</div>
+            <div className="px-[200px]">Hassan Raza</div>
+            <div className="px-[200px]"> Professional Fullstack developer</div>
+          </div>
+          <div>
+            <Image className="m-auto  py-[20px] " src="/img/pic.png" alt="Profile pic"  width={200} height={150}/>
+          </div>
+        </div> */}
       </section>
       <section
         id="about"
@@ -27,7 +74,7 @@ export default function Home() {
         className="text-white h-screen bg-black  text-center flex"
       >
         <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">
-          Contact{' '}
+          Contact{" "}
         </h1>
       </section>
       <section
